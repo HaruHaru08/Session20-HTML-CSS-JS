760fc60a513780eb2dbf3ec0fcdecc7561110856
HaruHaru <cuong33442@gmail.com>
2025-03-05 16:37:36 +0700

    first commmit

--- b8.js
+++ b8.js
@@ -1,21 +1,22 @@
-let number = +prompt("Nhập vào một số bất kỳ: ");
-if (number < 0 || isNaN(number)) {
+let num = +prompt("Nhập vào một số nguyên bất kỳ: ");
+if (num < 0 || isNaN(num)) {
     console.log("không hợp lệ");
 } else {
     let count = 0;
     let index = 2;
-    while (count < number) {
-        let isPrime = true;
+    while (count < num) {
+        let isPrine = true;
         for (let i = 2; i <= Math.sqrt(index); i++) {
             if (index % i === 0) {
                 isPrine = false;
                 break;
             }
         }
-        if (isPrime) {
+        if (isPrine) {
             console.log(index);
             count++;
         } 
         index++;
+
     }
 }
